4ca3851657f0c3976abf7a046962c030b5af033f
Marc 'Judge' Richter <mail@marc-richter.info>
2015-08-31 21:52:03 +0200

    finalized CI build step 01

--- build_steps/01_Create_JB.sh
+++ build_steps/01_Create_JB.sh
@@ -1,14 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 # Load constants
 if [ -e $(dirname $(readlink -f $0))/constants ]; then
     . $(dirname $(readlink -f $0))/constants
 fi
 
 # Create JB
-#result="$(curl -d name=${BUILD_ID} -d planid=20 -d distribution=debian_jessie_64bit -d use_sshkey ${base_url})"
-result="$(curl -d name=dskdjfndj -d planid=20 -d distribution=debian_jessie_64bit -d use_sshkey ${base_url})"
+result="$(curl -d name=${BUILD_ID} -d planid=20 -d distribution=debian_jessie_64bit -d use_sshkey ${base_url})"
 echo ${base_url}
-if [[ "${result}" !=~ .*status.*CREATING.* ]]; then
-    echo "Success"
+if [ ! -z "echo ${result} | egrep ',"status":"CREATING"'" ]; then
+    echo "Couldn't be started properly."
+    exit 1
 fi
-echo "${result}"
