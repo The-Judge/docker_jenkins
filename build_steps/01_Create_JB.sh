74c80b8be9496bbeee90e43e8a68b5b95ad98724
Marc 'Judge' Richter <mail@marc-richter.info>
2015-08-31 21:35:16 +0200

    Commit to test

--- build_steps/01_Create_JB.sh
+++ build_steps/01_Create_JB.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 # Load constants
-if [ -e $(dirname $(readlink -f $0))/constats ]; then
-    source $(dirname $(readlink -f $0))/constants
-    echo "Found"
+if [ -e $(dirname $(readlink -f $0))/constants ]; then
+    . $(dirname $(readlink -f $0))/constants
 fi
 
 # Create JB
 #result="$(curl -d name=${BUILD_ID} -d planid=20 -d distribution=debian_jessie_64bit -d use_sshkey ${base_url})"
 result="$(curl -d name=dskdjfndj -d planid=20 -d distribution=debian_jessie_64bit -d use_sshkey ${base_url})"
+echo ${base_url}
 if [ "${result}" != "*status*CREATING" ]; then
     echo "Success"
 fi
