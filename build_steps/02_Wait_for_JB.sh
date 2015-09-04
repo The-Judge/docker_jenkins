4c4ee51cecbee3d6f083d6e3fe68d5c5cb769574
Marc Richter <mail@marc-richter.info>
2015-09-04 10:45:48 +0200

    Fix für Build Step 02

--- build_steps/02_Wait_for_JB.sh
+++ build_steps/02_Wait_for_JB.sh
@@ -10,12 +10,17 @@ load_const
 # Maximale Wartezeit, die die JB hochfahren darf in Sekunden
 WAIT=300
 
-wait_for_status ${WAIT} RUNNING
-wait_for_running ${WAIT} true
+function state {
+    if [ $? -ne 0 ]; then
+        echo "Build-Umgebung konnte nicht sauber gestartet werden."
+        return 1
+    else
+        echo "Build-Umgebung wurde gestartet."
+        return 0
+    fi
+}
 
-if [ $? -ne 0 ]; then
-    echo "Build-Umgebung konnte nicht sauber gestartet werden."
-    exit 1
-else
-    echo "Build-Umgebung wurde gestartet."
-fi
+wait_for_status ${WAIT} RUNNING
+state || exit 1
+wait_for_running ${WAIT} true
+state || exit 1
